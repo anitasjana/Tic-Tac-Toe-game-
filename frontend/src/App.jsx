273cfed2e266e46ba1dc1fe02a6e1d66821ecf92
Anitas jana <anitasjana1@gmail.com>
2026-09-18 10:42:41 +0530

    Build Tic-Tac-Toe game with React UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,122 +1,80 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0)
+  const [board,setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
 
+  const [currentPlayer,setCurrentPlayer]=useState("X");
+  const [winner,setWinner]=useState(null);
+
+  const resetGame = () => {
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setCurrentPlayer("X");
+    setWinner(null);
+  };
+  const handleClick = (index)=>{
+      if(winner!=null){
+        return ;
+      }
+      if (board[index] != "") {
+        return;
+      }
+
+     const newBoard = board.map( 
+        (value,i)=> i === index ? currentPlayer : value 
+      );
+
+      setBoard(newBoard);    
+      const result = checkWinner(newBoard);
+
+      if(result){
+        setWinner(result);
+        return;
+      }
+      if (!newBoard.includes("")) {
+        setWinner("Draw");
+        return;
+      }
+      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  }
+  const checkWinner = (board) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for(const [a, b, c] of lines){
+      if( board[a]!="" && board[a]===board[b] && board[a]==board[c] ) {
+        return board[a];
+      }
+    }
+  };
+ 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="board">
+      <h1>Tic-Tac-Toe</h1>
+      {winner && <h1>{winner === "Draw" ? "Draw" : `${winner} wins`}  </h1>}
+      {winner && <button onClick={resetGame}>Restart Game</button>}
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+      <div>
+        <button onClick={ ()=> handleClick(0) }>{board[0]}</button>
+        <button onClick={ ()=> handleClick(1) }>{board[1]}</button>
+        <button onClick={ ()=> handleClick(2) }>{board[2]}</button>
+        <br />
+        <button onClick={ ()=> handleClick(3) }>{board[3]}</button>
+        <button onClick={ ()=> handleClick(4) }>{board[4]}</button>
+        <button onClick={ ()=> handleClick(5) }>{board[5]}</button>
+        <br />
+        <button onClick={ ()=> handleClick(6) }>{board[6]}</button>
+        <button onClick={ ()=> handleClick(7) }>{board[7]}</button>
+        <button onClick={ ()=> handleClick(8)}>{board[8]}</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
