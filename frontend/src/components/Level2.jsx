668439ca9ed42c9c4c5854b22d4cba099efae9ef
Anitas jana <anitasjana1@gmail.com>
2026-09-24 01:25:42 +0530

    Add Level 2 turn system

--- frontend/src/components/Level2.jsx
+++ frontend/src/components/Level2.jsx
@@ -2,15 +2,27 @@ import { useState } from "react"
 
 const Level2 = () => {
     const [board, setBoard] = useState(["", "", "", "", "", "", "", "",""]);
+    const [currentPlayer, setCurrentPlayer] = useState("X");
+
+    const handleClick = (index) => {
+    if (board[index] !== "") {
+        return;
+    }
+    const newBoard = board.map(
+        (cell, i) => i === index ? currentPlayer : cell
+    );
+
+    setBoard(newBoard);
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+    };
+
   return (
     <div>
       <h1>Level 2</h1>
 
       <div className="board">
         {board.map((cell, index) => (
-          <button key={index} className="cell">
-            {cell}
-          </button>
+          <button key={index} className="cell" onClick={() => handleClick(index)} > {cell} </button>           
         ))}
       </div>
     </div>
